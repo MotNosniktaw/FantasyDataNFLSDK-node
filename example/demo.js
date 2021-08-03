65ce455f1beb11f4c806f9eaf39958ce791d0038
MotNosniktaw <tomwatkinson90gmail.com>
2021-08-03 17:59:01 +0100

    remove place holder api key

--- example/demo.js
+++ example/demo.js
@@ -1,4 +1,4 @@
-const client = require("../src/index")("fe3b29c24c9a4fb69eca5941eebb3300");
+const client = require("../src/index")("REPLACE WITH API KEY");
 const main = async () => {
   const getPlayersResult = await client.getPlayerDetails();
   console.log({ getPlayersResult });
